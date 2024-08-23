21271db6c3d8c136d6ac4c49b3100ab7f9f6a5e5
jduru213-1 <jduru213@gmail.com>
2024-08-22 22:29:40 -0400

    Yo

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import ProductCard from "./components/ProductCard";
 export default function Home() {
   return (
     <main>
-      <h1> Hello World </h1> 
+      <h1> Hello World Yo yo yo </h1> 
       <h2> Testing the source control</h2>
       <Link href = "/users">Users</Link>
       <ProductCard />
