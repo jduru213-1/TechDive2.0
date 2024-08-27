b6bad5bc5f56196470172fb44ab337b50beed448
jduru213-1 <jduru213@gmail.com>
2024-08-26 21:42:39 -0400

    Updated the global files

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,12 @@ import ProductCard from "./components/ProductCard";
 export default function Home() {
   return (
     <main>
-      <h1> Hello World Yo yo yo </h1> 
+      <h1> Hello World </h1> 
       <h2> Testing the source control</h2>
+      <h2> Hello World</h2>
+      <h3> Hey man </h3>
+      <h4> Yo bro lets get this app ruuning </h4>
+
       <Link href = "/users">Users</Link>
       <ProductCard />
     </main>
