3e35cb621c66709d01767b587945f2451c04e0c2
jduru213-1 <jduru213@gmail.com>
2024-09-02 06:58:26 -0400

    Most Current verzion - worked on structure abd commenting - added animations - started on resource section and want to create a card componet (https://www.youtube.com/watch?v=Tkp3FDgOueM&t=1701s) (28:35)

--- app/page.tsx
+++ app/page.tsx
@@ -20,60 +20,79 @@ export default function Home() {
         />
 
         {/* Main Content */}
-        <div className={styles.contents}>
-          <h1>Discover the Future of Tech<br />Unlock New Possibilities
-          </h1>
-          <p>A platform and resource designed by students, for students and tech enthusiasts.<br />
+      <div className={styles.contents}>
+       <h1>Discover the Future of Tech<br />Unlock New Possibilities</h1>
+        <p>A platform and resource designed by students, for students and tech enthusiasts.<br />
           Stay informed with the latest tech news from various sources and dive deeper<br />
           into tech concepts and projects that shape our world and its future.
-          </p>
+        </p>
           
 
           {/* Buttons */}
-          <div className={styles.button}>
-            <button>About</button>
-            <button>Story</button>
-          </div>
-          
-          <div className={styles.motive}>
-            <h1>
-          The Motive
-            </h1>
-          <p>TechDive was created during our internship in DC as a platform for students and 
-           tech enthusiasts to progressively learn about key concepts in technology.  <br />We saw the need for a space that not only combines tech news, projects, and educational resources 
-          but also serves as a way for us to research and stay current in an ever-evolving field.
-          </p>
-          </div>
+      <div className={styles.button}>
+        <button>About</button>
+           <button>
+              <a href="#motive-section">Story</a>
+          </button>
+      </div>
+
+        {/* Motive Section */}
+      <div>
+        <div id="motive-section" className={styles.motive}>
+            <h1>The Motive</h1>
+              <p>TechDive was created during our internship in DC as a platform for students and 
+              tech enthusiasts to progressively learn about key concepts in technology.  <br />We saw the need for a space that not only combines tech news, projects, and educational resources 
+              also serves as a way for us to research and stay current in an ever-evolving field.
+             </p>
         </div>
-                
+      </div>
+    </div>
+             
         {/* Navigation */}
-        <header className={styles.header}>
+      <header className={styles.header}>
           <a href="#" className={styles.logo}>TECH DIVE</a>
-
-          <nav className={styles.navbar}>
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Story</a>
-            <a href="#">Outlets</a>
+            <nav className={styles.navbar}>
+              <a href="#">Home</a>
+              <a href="#">About</a>
+              <a href="#resources-section">Resources</a>
+             <a href="#">Outlets</a>
           </nav>
-        </header>
+      </header>
 
-{/* Getting our websites on the page */}
-        <section>
-        <div className={styles.Justin}>
-        <img src="/images/Justin.jpg"
-        width={400}
-        height={600}
-        />
-        </div>
-
+{/* Getting our images on the page and button beneath it */}
+      <div>
+        
+      {/* Justin section container */}
+      {/* Image and button */}
+        <Link href='/users'>
+          <div className={styles.Justin} >
+            <div className={styles.imgContainer}>
+              <img src="/images/Justin.jpg" width={175} height={175} alt="Justin Duru" />
+              <div className={styles.textbox}>
+              <button>View Profile</button> 
+              </div>
+            </div>
+          </div>
+        </Link>
+        
+      {/* Courage section container */}
+      {/* Image and button */}
         <div className={styles.courage}>
-          <img src="/images/courage.jpg"
-          width={400}
-          height={600} />
-
+          <div className={styles.imgContainer2}>
+            <img src="/images/courage.jpg" width={175} height={175} alt="Courage Tikum" />
+            <div className={styles.textbox2}>
+              <button>View Profile</button>
+            </div>
+          </div>
         </div>
-        </section>      
+
+      {/* Resources section container */}
+         <div>
+           <div id="resources-section"  className={styles.resources}>
+            <h1>Resources</h1>
+              </div>
+            </div>
+          </div>
       </main>
     </>
   );
